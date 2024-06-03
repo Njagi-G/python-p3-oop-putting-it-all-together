428e338656b274d59af5026f34e4c40f97c06844
Njagi Githaiga <njagigithaiga@gmail.com>
2024-06-03 17:41:47 +0300

    Add attributes, properties and methods to Book class and Shoe class

--- lib/book.py
+++ lib/book.py
@@ -2,5 +2,59 @@
 
 class Book:
     pass
+    def __init__(self, title = "And Then There Were None", page_count = 272):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        """The page count property"""
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, page_count):
+        "page_count must be an integer"
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+bible = Book(title = "King James Version", page_count = 1360)
+print(bible.title)
+print(bible. page_count)
+bible.turn_page()
+
+geeks_for_geeks = Book()
+print(geeks_for_geeks.title)
+print(geeks_for_geeks.page_count)
+geeks_for_geeks.turn_page()
+
+geeks_for_geeks.title = "Python for Beginners"
+print(geeks_for_geeks.title)
+
+geeks_for_geeks.page_count = "12"
+print(geeks_for_geeks.page_count)
+
+setattr(geeks_for_geeks, "title", "JavaScript for Dummies")
+print(getattr(geeks_for_geeks, "title"))
+
+
+    
+
+
+
+    
+
+    
+
+
+
+    
+
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -2,3 +2,56 @@
 
 class Shoe:
     pass
+    def __init__(self, brand = "Adidas", size = 9):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def size(self):
+        """The size property"""
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        """size must be an integer"""
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self, condition = "New"):
+        self.condition = condition
+        print("Your shoe is as good as new!")
+
+
+dwayne_wayde = Shoe()
+print(dwayne_wayde.brand)
+print(dwayne_wayde.size)
+
+jordan = Shoe("Nike", 16)
+print(jordan.brand)
+print(jordan.size)
+
+maupay = Shoe("Umbro", "ten")
+print(maupay.brand)
+
+setattr(maupay, "size", 10)
+print(maupay.size)
+
+print(getattr(maupay, "brand"))
+
+print(hasattr(maupay, "brand"))
+
+delattr(maupay, "brand")
+print(hasattr(maupay, "brand"))
+
+
+
+    
+
+    
+
+
+        
+
+
